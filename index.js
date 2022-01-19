f97173454923812d05124e25111d450edb03c9a7
ivan <blockbench@gmail.com>
2022-01-19 16:20:02 +0200

    clear

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-const commit = 4444
-=======
-const rebase = "conflict"
->>>>>>> conflict
-=======
-const rebase = "conflict"
->>>>>>> 3f2dd611f65aa150dc88318f03a77f920d691d35
+"clear"
